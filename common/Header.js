585b63c4797b10bb1c7a0c387d3f283122de9eb8
Sharma, Ravi <Ravi1.Sharma@nttdata.com>
2022-11-19 17:28:05 +0530

    Header.js updated for good design

--- common/Header.js
+++ common/Header.js
@@ -2,15 +2,28 @@ import React from 'react';
 import Head1 from './Head1';
 import Head2 from './Head2';
 import Head3 from './Head3';
+import Navbar from './Navbar';
 
 const Header = () => {
   return (
     <div className='bg-success bg-opacity-50'>
       <div class="container">
         <div class="row">
-          <Head1/>
+          {/* <Head1/>
           <Head2/>
-          <Head3/>
+          <Head3/> */}
+          <div class="col-2">
+            <Head1/>
+          </div>
+          <div class="col-5">
+            <Navbar />
+          </div>
+          <div class="col-3">
+
+          </div>
+          <div class="col-2">
+
+          </div>
         </div>        
       </div>
     </div>
