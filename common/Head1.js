ee0db5d6fc73cd28b75a9209b57f3b2e8ad01259
Sharma, Ravi <Ravi1.Sharma@nttdata.com>
2022-11-19 17:18:29 +0530

    Header myntra logo updated

--- common/Head1.js
+++ common/Head1.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Header from '../common/Header';
+import Footer from '../common/Footer';
+import Navbar from '../common/Navbar';
+import Image from 'next/image';
+import Head from 'next/head';
 
 const Head1 = () => {
   return (
     <div className='col-6'>
-        <h2>ABC School</h2>
+        <Image src='/logo.png' width={75} height={50}/>
     </div>
   )
 }
