80bc33f6db555998fb9ede67aed54d4adbe482bb
Sharma, Ravi <Ravi1.Sharma@nttdata.com>
2022-11-20 11:19:00 +0530

    global.css | Head3.js | Logincta.module.css updated for good design

--- common/Head3.js
+++ common/Head3.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles/Logincta.module.css';
 
 const Head3 = () => {
   return (
     <div>
         <form action="#" method="post">
-          <button type="submit">Login</button>
+          <button type="submit" className={styles.login_cta}>Login</button>
         </form>
     </div>
   )
