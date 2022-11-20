dc25d5ed443f8d0bc81a9a28ec1c95a935d58f17
Sharma, Ravi <Ravi1.Sharma@nttdata.com>
2022-11-20 11:07:23 +0530

    Head2.js | search.png icon updated

--- common/Head2.js
+++ common/Head2.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from '../styles/Search.module.css';
 
 const Head2 = () => {
@@ -6,6 +7,7 @@ const Head2 = () => {
     <div>
       {/* <h2>search bar</h2> */}
       <form action="#" method="post">
+        <Image src='/search.png' width={20} height={20}/>
         <input type="text" id="search-bar"  
         className={styles.search_text} 
         name="search-bar" 
