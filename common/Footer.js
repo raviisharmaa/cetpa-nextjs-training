80d9c152f1bd03a7421a04a95e82040ba4feba46
Sharma, Ravi <Ravi1.Sharma@nttdata.com>
2022-11-20 11:31:52 +0530

    Footer.js | Footer.module.css updated for good design

--- common/Footer.js
+++ common/Footer.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles/Footer.module.css';
 
 const Footer = () => {
   return (
-    <div className='bg-info bg-opacity-50'>
+    <div className={styles.footer}>
       <div class="container">
         <div class="row">
           <div class="col-sm-6">
               <h6>In case of any concern, Contact Us</h6>
           </div>
           <div class="col-sm-6">
-              <h6>© 2022 www.myntra.com. All rights reserved.</h6>
+              <h6 className={styles.reserve}>© 2022 www.myntra.com. All rights reserved.</h6>
           </div>
         </div>
       </div>        
