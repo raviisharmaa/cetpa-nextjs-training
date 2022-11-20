79dd5ed84cbc862f24bc1cb540968a1efcafa1e8
Sharma, Ravi <Ravi1.Sharma@nttdata.com>
2022-11-20 11:25:33 +0530

    Footer.js content updated

--- common/Footer.js
+++ common/Footer.js
@@ -5,7 +5,12 @@ const Footer = () => {
     <div className='bg-info bg-opacity-50'>
       <div class="container">
         <div class="row">
-          <h3>Copyright</h3>
+          <div class="col-sm-6">
+              <h6>In case of any concern, Contact Us</h6>
+          </div>
+          <div class="col-sm-6">
+              <h6>© 2022 www.myntra.com. All rights reserved.</h6>
+          </div>
         </div>
       </div>        
     </div>
