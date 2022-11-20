0fe40bd7eef07ab2d3db7d35329a71460b843e72
Sharma, Ravi <Ravi1.Sharma@nttdata.com>
2022-11-20 10:55:40 +0530

    Search.module.css | Head2.js searchbar text and css updated

--- common/Head2.js
+++ common/Head2.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles/Search.module.css';
 
 const Head2 = () => {
   return (
     <div>
       {/* <h2>search bar</h2> */}
       <form action="#" method="post">
-        <input type="text" id="search-bar" name="search-bar" placeholder="searchbar" />
+        <input type="text" id="search-bar"  
+        className={styles.search_text} 
+        name="search-bar" 
+        placeholder="Search for products" />
       </form>
     </div>
   )
