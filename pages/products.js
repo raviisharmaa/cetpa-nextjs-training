a1c4bfa36d6184854a025fd80fa62500e430da21
Sharma, Ravi <Ravi1.Sharma@nttdata.com>
2022-11-21 12:31:28 +0530

    products.js | ProductItem.module.css updated for good design using API data only

--- pages/products.js
+++ pages/products.js
@@ -1,197 +1,42 @@
-import React from 'react';
-import Header from '../common/Header';
-import Footer from '../common/Footer';
-import Navbar from '../common/Navbar';
-import styles from '../styles/ProductItem.module.css';
+import React from "react";
 import Image from 'next/image';
-import Head from 'next/head';
-import productitem from '../component/productitem';
+import style from '../styles/ProductItem.module.css';
 
 export const getStaticProps = async () => {
-    //api call
-    const response = await fetch('https://fakestoreapi.com/products')
-    const data = await response.json();  // to convert stringified json to parsed json
+    const response = await fetch ('https://fakestoreapi.com/products');
+    const data = await response.json(); //To convert stringify json to parsed json
     return {
-        props: {productData: data}
+        props : {productData : data}
     }
-}
+}   
 
-const Products = (props) => {
-    
+const Product = (props) => {
     const {productData} = props;
     return(
-        <div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-3">
-                        <div className={styles.filter_view}>
-                            Upcoming filter area
-                        </div>                        
-                    </div>
-                    <div className="col-sm-9">
-                        <div className="row">
-                            <div className="col-sm-3">
-                                <productitem />
-                                
-                                {/* <div  className={styles.product_view}>
-                                    <div className="product-img">
-                                        <Image src='/bag.jpg' width={175} height={250} alt="product"/>
-                                    </div>
-                                    <div className="product-details">
-                                        <h3 className={styles.product_title}>american tourister</h3>
-                                        <h4 className={styles.product_desc}>Kansas 57 Cabin Trolley</h4>
-                                        <div>
-                                            <span className={styles.product_rp}>Rs. 2799</span> 
-                                            <span className={styles.product_dp}>Rs 7180</span> 
-                                            <span className={styles.product_off}>(Rs 4381 OFF)</span> 
-                                        </div>
-                                    </div>
-                                </div> */}
-
-                            </div>
-                            <div className="col-sm-3">
-                                {/* <productitem /> */}
-
-                                <div  className={styles.product_view}>
-                                    <div className="product-img">
-                                        <Image src='/bag.jpg' width={175} height={250} alt="product"/>
-                                    </div>
-                                    <div className="product-details">
-                                        <h3 className={styles.product_title}>american tourister</h3>
-                                        <h4 className={styles.product_desc}>Kansas 57 Cabin Trolley</h4>
-                                        <div>
-                                            <span className={styles.product_rp}>Rs. 2799</span> 
-                                            <span className={styles.product_dp}>Rs 7180</span> 
-                                            <span className={styles.product_off}>(Rs 4381 OFF)</span> 
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col-sm-3">
-                                {/* <productitem /> */}
-
-                                <div  className={styles.product_view}>
-                                    <div className="product-img">
-                                        <Image src='/bag.jpg' width={175} height={250} alt="product"/>
-                                    </div>
-                                    <div className="product-details">
-                                        <h3 className={styles.product_title}>american tourister</h3>
-                                        <h4 className={styles.product_desc}>Kansas 57 Cabin Trolley</h4>
-                                        <div>
-                                            <span className={styles.product_rp}>Rs. 2799</span> 
-                                            <span className={styles.product_dp}>Rs 7180</span> 
-                                            <span className={styles.product_off}>(Rs 4381 OFF)</span> 
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col-sm-3">
-                                {/* <productitem /> */}
-
-                                <div className={styles.product_view}>
-                                    <div className="product-img">
-                                        <Image src='/bag.jpg' width={175} height={250} alt="product"/>
-                                    </div>
-                                    <div className="product-details">
-                                        <h3 className={styles.product_title}>american tourister</h3>
-                                        <h4 className={styles.product_desc}>Kansas 57 Cabin Trolley</h4>
-                                        <div>
-                                            <span className={styles.product_rp}>Rs. 2799</span> 
-                                            <span className={styles.product_dp}>Rs 7180</span> 
-                                            <span className={styles.product_off}>(Rs 4381 OFF)</span> 
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-
-                            <div className="col-sm-3">
-                                {/* <productitem /> */}
-
-                                <div className={styles.product_view}>
-                                    <div className="product-img">
-                                        <Image src='/bag.jpg' width={175} height={250} alt="product"/>
-                                    </div>
-                                    <div className="product-details">
-                                        <h3 className={styles.product_title}>american tourister</h3>
-                                        <h4 className={styles.product_desc}>Kansas 57 Cabin Trolley</h4>
-                                        <div>
-                                            <span className={styles.product_rp}>Rs. 2799</span> 
-                                            <span className={styles.product_dp}>Rs 7180</span> 
-                                            <span className={styles.product_off}>(Rs 4381 OFF)</span> 
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-
-                            <div className="col-sm-3">
-                                {/* <productitem /> */}
-
-                                <div className={styles.product_view}>
-                                    <div className="product-img">
-                                        <Image src='/bag.jpg' width={175} height={250} alt="product"/>
-                                    </div>
-                                    <div className="product-details">
-                                        <h3 className={styles.product_title}>american tourister</h3>
-                                        <h4 className={styles.product_desc}>Kansas 57 Cabin Trolley</h4>
-                                        <div>
-                                            <span className={styles.product_rp}>Rs. 2799</span> 
-                                            <span className={styles.product_dp}>Rs 7180</span> 
-                                            <span className={styles.product_off}>(Rs 4381 OFF)</span> 
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-
-                            <div className="col-sm-3">
-                                {/* <productitem /> */}
-
-                                <div className={styles.product_view}>
-                                    <div className="product-img">
-                                        <Image src='/bag.jpg' width={175} height={250} alt="product"/>
-                                    </div>
-                                    <div className="product-details">
-                                        <h3 className={styles.product_title}>american tourister</h3>
-                                        <h4 className={styles.product_desc}>Kansas 57 Cabin Trolley</h4>
-                                        <div>
-                                            <span className={styles.product_rp}>Rs. 2799</span> 
-                                            <span className={styles.product_dp}>Rs 7180</span> 
-                                            <span className={styles.product_off}>(Rs 4381 OFF)</span> 
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-
-                            <div className="col-sm-3">
-                                {/* <productitem /> */}
-
-                                <div className={styles.product_view}>
-                                    <div className="product-img">
-                                        <Image src='/bag.jpg' width={175} height={250} alt="product"/>
-                                    </div>
-                                    <div className="product-details">
-                                        <h3 className={styles.product_title}>american tourister</h3>
-                                        <h4 className={styles.product_desc}>Kansas 57 Cabin Trolley</h4>
-                                        <div>
-                                            <span className={styles.product_rp}>Rs. 2799</span> 
-                                            <span className={styles.product_dp}>Rs 7180</span> 
-                                            <span className={styles.product_off}>(Rs 4381 OFF)</span> 
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
+        <div className = "container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <div className={style.filter_view}>
+                        Upcoming filter area
                     </div>
                 </div>
-            </div>
+                <div class="col-sm-9">
+                    <div class="row">
+                        {productData.map(item => (
+                            <div className={[style.product_view, "col-sm-3"].join(" ")} key={item.id}>
+                                <div className = {style.product_wrap}>
+                                    <Image className = {style.product_img} src="/bag.jpg" width={185} height={200} alt={item.title}></Image>
+                                    <h3 className = {style.product_title}>{item.title}</h3>
+                                    <h4 className = {style.product_desc}>{item.description}</h4>
+                                    <h5 className = {style.product_rp}>{"Rs." + item.price}</h5>
+                                </div>
+                            </div>
+                        ))}
+                    </div>                    
+                </div>
+            </div>            
         </div>
     )
 }
 
-export default Products;
+export default Product;
